26f6f4dde649bf1ce67f9d6ad21ec5d98baad26d
Ernest Loveland <edg3@users.noreply.github.com>
2018-04-12 20:58:56 +0200

    Logic for one away structure initialised

---
+++ src/Morabaraba-9001/GameCode/OneAway.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Morabaraba_9001.GameCode
+{
+    public class OneAway
+    {
+        private static List<OneAway> _opciones;
+        public static List<OneAway> Options
+        {
+            get
+            {
+                if (_opciones == null)
+                {
+                    _opciones = new List<OneAway>();
+                    _opciones.Add(new OneAway(BoardPos.a1, new List<BoardPos> { BoardPos.a4, BoardPos.b2, BoardPos.d1 }));   //still need to create oneaway constructor to have its 
+
+                    //TO BE COMPLETED
+                }
+                return _opciones;
+
+            }
+        }
+
+        public BoardPos Bigme { get; private set; }
+        public List<BoardPos> Spot { get; private set; }
+
+        public OneAway(BoardPos me , List<BoardPos> spots)
+        {
+            Bigme = me;
+            Spot = spots;
+        }
+    }
+}
