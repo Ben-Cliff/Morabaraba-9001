16968e01a61851a62f22233e88362d3a42303926
g15l2589 <loveland.ernest@gmail.com>
2018-04-12 16:12:05 +0200

    Added set of mills data structure

--- src/Morabaraba-9001/GameCode/BoardPositions.cs
+++ src/Morabaraba-9001/GameCode/BoardPositions.cs
@@ -6,12 +6,12 @@ namespace Morabaraba_9001.GameCode
 {
     public enum BoardPos
     {
-        a1, a4, a7,
-        b2, b4, b6,
-        c3, c4, c5,
-        d1, d2, d3, d5, d6, d7,
-        e3, e4, e5,
-        f2, f4, f6,
-        g1, g4, g7
+        a1,                    a4,                         a7,
+                b2,            b4,                  b6,
+                           c3, c4, c5,
+        d1,     d2,        d3,     d5,              d6,     d7,
+                           e3, e4, e5,
+                f2,            f4,                  f6,
+        g1,                    g4,                          g7
     }
 }
