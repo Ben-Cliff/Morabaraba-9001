55087837d15cd113c34b28fc8bac906406efe905
BenTrope <benjamintrollope@gmail.com>
2018-04-17 23:10:51 +0200

    Test suite initialised, framework set

--- src/Morab/Morabaraba-9001.Test/Tests.cs
+++ src/Morab/Morabaraba-9001.Test/Tests.cs
@@ -10,12 +10,88 @@ namespace Morabaraba_9001.Test
     [TestFixture]
     public class Tests
     {
-        [Test]
+
+        /*
+            FIRST PRINCPLES:
+                -> FAST
+                -> ISOLATED
+                -> REPEATABLE
+                ->THROUGH
+         */
+
+
+        [Test]//Checking initial debug mode. 7 will be replaced with 12 and vica verca
         public void PlayersStartAndCanPlace7Cows()
         {
             var player1 = new Player(Player.Type.Red, 7);
             // Note we used 7 to test if this works in testing with a smaller herd
             Assert.That(player1.CowsLeftToPlace == 7);
         }
+
+
+        [Test]
+        public void GameBoardEmptyAtStart()
+        {
+
+
+        }
+
+        /*
+
+        [Test] //Dark player start
+        public void RedShouldStart()
+        {
+
+
+        }
+
+
+        [Test]
+        public void OnlyEmptySpots()
+        {
+
+
+        }
+
+
+        [Test]
+        public void MaximumTwelvePlacements()
+        {
+
+
+        }
+
+
+
+        [Test]
+        public void TestMoveStateWhilePlacing()
+        {
+
+
+        }
+
+    */
+        //GENERAL
+
+
+        [Test]
+        public void MillMadeOfSameHerd()
+        {
+            
+
+        }
+
+
+        [Test]
+        public void TwoPlayersExist()
+        {
+           
+
+
+        }
+
+
+
+
     }
 }
