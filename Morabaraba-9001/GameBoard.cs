9f651940e1f05f2219b5583aa3a01d0682ed4274
edg3 <loveland.ernest@gmail.com>
2018-04-21 15:08:33 +0200

    Implemented starter gameboard to help show team

---
+++ Morabaraba-9001/GameBoard.cs
@@ -0,0 +1,95 @@
+﻿using Morabaraba9001.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*
+ * How to create an instance of the interface:
+ *  - Create new class
+ *  - Inherit from the interface be adding requirements -> e.g. ": IBoard" for this one
+ *    - e.g:
+ *      -> Add text " : IBoard" after "public class GameBoard"
+ *      -> Put the cursor (marker where you are typing) inside IBoard, press "crtl + ."
+ *          -> implement interface is what you select
+ *      -> For the data we added to the interface (e.g. Positions) here is what it makes:
+ *         public List<Colour> Positions => throw new NotImplementedException();
+ *         This is called an "accessor" that is implemented "without block body"
+ *          -> more on that "settings" -> "text editor" -> "c#" -> "code style" -> "general"
+ *         We wont need more on that, we replace " => throw new NotImplementedException();" with " { get; private set; }"
+ *         In the past I was making a private instance but this isn't needed currently
+ *      -> lastly create the constructor sharing what it needs to inherit
+ *         -> remember the structure needed for potential tests
+ *         -> e.g. the board takes a list of the current board, an empty list = an empty board
+ *         -> In the constructor create the necessary data thats needed
+ *           -> Note: Notes will be using "Note:" and can be potential changes, adjustments and/or fixes we want in the project, this just maps them in our code for us
+ */
+
+namespace Morabaraba9001
+{
+    public class GameBoard : IBoard
+    {
+        public List<Colour> Positions { get; private set; }
+
+        public List<List<int>> Mills { get; private set; }
+
+        public GameBoard(List<Colour> newPositions)
+        {
+            Positions = newPositions;
+
+            // Create the mills this board uses
+            // Note: should we also input it like the board's current state for testing? Since technically we never change mills.
+            Mills = new List<List<int>>();
+
+            // Horizontal
+            Mills.Add(new List<int>() { 0, 1, 2 });
+            Mills.Add(new List<int>() { 8, 9, 10});
+            Mills.Add(new List<int>() { 16, 17, 18 });
+            Mills.Add(new List<int>() { 7, 15, 23 });
+            Mills.Add(new List<int>() { 19, 11, 3 });
+            Mills.Add(new List<int>() { 22, 21, 20 });
+            Mills.Add(new List<int>() { 14, 13, 12 });
+            Mills.Add(new List<int>() { 6, 5, 4 });
+
+            // Vertical
+            Mills.Add(new List<int>() { 0, 7, 6 });
+            Mills.Add(new List<int>() { 8, 15, 14 });
+            Mills.Add(new List<int>() { 16, 23, 22 });
+            Mills.Add(new List<int>() { 1, 9, 17 });
+            Mills.Add(new List<int>() { 21, 13, 5 });
+            Mills.Add(new List<int>() { 18, 19, 20 });
+            Mills.Add(new List<int>() { 10, 11, 12 });
+            Mills.Add(new List<int>() { 2, 3, 4 });
+
+            // Diagonal
+            Mills.Add(new List<int>() { 0, 8, 16 });
+            Mills.Add(new List<int>() { 18, 10, 2 });
+            Mills.Add(new List<int>() { 6, 14, 22 });
+            Mills.Add(new List<int>() { 4, 12, 20 });
+        }
+
+        public bool AdjustBoard_Fly(int positionFrom, int positionTo, Colour player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AdjustBoard_Move(int positionFrom, int positionTo, Colour player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AdjustBoard_Place(int position, Colour player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AdjustBoard_Shoot(int targer, Colour player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int PlayerCowCount(Colour c)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Morabaraba-9001/Program.cs
+++ Morabaraba-9001/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Morabaraba9001.Interfaces;
+using System;
+using System.Collections.Generic;
 
 namespace Morabaraba9001
 {
@@ -6,7 +8,17 @@ namespace Morabaraba9001
     {
         static void Main(string[] args)
         {
+            // As you will note we need instances to use the game
+            // Currently this version isnt implemented however we still make sure we try use it
+            //  -> you will need to add references like for GameBoard these were added:
+            //       using Morabaraba9001.Interfaces;
+            //       using System.Collections.Generic;
+            List<Colour> board = new List<Colour>();
+            // Currently the board is empty to start, this is just since we test if it compiles and runs
+            GameBoard gameBoard = new GameBoard(board);
+
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
